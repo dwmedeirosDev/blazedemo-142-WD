40c900063274a5c5a3859936ad5cd0cb1ca84445
David Medeiros <davidmedeiros94@hotmail.com>
2025-01-31 17:01:41 -0300

    Imports, atributos e blocos

---
+++ src/test/java/PassagemTest.java
@@ -0,0 +1,35 @@
+// 1- Bibliotecas, imports...
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver; // Biblioteca principal do Selenium
+
+// 2- Classe 
+public class PassagemTest {
+
+    // 2.1- Atributos
+    private WebDriver driver; // Objeto do Selenium
+    
+
+    // 2.2- Métodos e funções
+
+    // Antes do teste
+    @BeforeEach
+    public void iniciar() {
+
+    }
+
+    // Depois do teste
+    @AfterEach
+    public void finalizar() {
+    
+    }
+
+    // Teste
+    @Test
+    public void comprarPassagem() {
+    
+    }
+
+    
+}
