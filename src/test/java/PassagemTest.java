bdc0e5e3597fc5f5e1d52eac91d910cca22feed1
David Medeiros <davidmedeiros94@hotmail.com>
2025-02-01 10:33:05 -0300

    adicionado assertequals, seletores, e comentários

--- src/test/java/PassagemTest.java
+++ src/test/java/PassagemTest.java
@@ -1,20 +1,21 @@
 // 1- Bibliotecas, imports...
 
 // Biblioteca do JUnit
-import org.junit.jupiter.api.AfterEach; 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test; 
+import static org.junit.jupiter.api.Assertions.assertEquals; // Biblioteca do JUnit para asserções
+import org.junit.jupiter.api.AfterEach; // Biblioteca do JUnit para métodos que são executados após cada teste
+import org.junit.jupiter.api.BeforeEach; // Biblioteca do JUnit para métodos que são executados antes de cada teste
+import org.junit.jupiter.api.Test; // Biblioteca do JUnit para métodos de teste
 
 import org.openqa.selenium.By; // Biblioteca do Selenium para localizar elementos na página
 import org.openqa.selenium.WebDriver; // Biblioteca principal do Selenium
-// import org.openqa.selenium.WebElement; // Biblioteca do Selenium para elementos da página
+// import org.openqa.selenium.WebElement; // import org.openqa.selenium.WebElement; // Biblioteca do Selenium para elementos da página
 import org.openqa.selenium.chrome.ChromeDriver; // Biblioteca do ChromeDriver
 
 // 2- Classe 
 public class PassagemTest {
 
     // 2.1- Atributos
-    private WebDriver driver; // Objeto do Selenium
+    private WebDriver driver; // Atributo do Selenium WebDriver
     
 
     // 2.2- Métodos e funções
@@ -71,18 +72,20 @@ public class PassagemTest {
 
         // Jeito simplificado &&
         // Selecionar o dropdown (Menu suspenso) de origem e destino e clicar no botão Find Flights
-        {
-            driver.findElement(By.name("fromPort")).click();
-            driver.findElement(By.xpath("//option[. = \"São Paolo\"]")).click();
-        }
+        driver.findElement(By.name("fromPort")).click(); // Clicar no campo de origem
+        driver.findElement(By.xpath("//option[. = \"São Paolo\"]")).click(); // Selecionar a opção São Paolo e clicar
+        driver.findElement(By.name("toPort")).click(); // Clicar no campo de destino
+        driver.findElement(By.xpath("//option[. = \"New York\"]")).click(); // Selecionar a opção New York e clicar
+        driver.findElement(By.cssSelector("input.btn.btn-primary")).click(); // Clicar no botão Find Flights
+        //Os principais métodos de localização de elementos são: By.id, By.name, By.className, By.tagName, By.xpath, By.cssSelector
+     
+        // Transição de página
+        
+        // assertEquals é uma função do JUnit que verifica se o valor esperado é igual ao valor retornado
+        assertEquals("Flights from São Paolo to New York:", driver.findElement(By.cssSelector("h3")).getText()); // Verificar se a mensagem apareceu
+        driver.findElement(By.cssSelector("tr:nth-child(1)")).click(); // Clicar no botão Choose This Flight
 
-        {
-            driver.findElement(By.name("toPort")).click();
-            driver.findElement(By.xpath("//option[. = \"New York\"]")).click();
-        }
-
-        {
-            driver.findElement(By.cssSelector("input.btn.btn-primary")).click();
-        }    
+        // Transição de página
+        assertEquals("Your flight from TLV to SFO has been reserved.", driver.findElement(By.cssSelector("h2")).getText()); // Verificar se a mensagem apareceu
     }
 }
